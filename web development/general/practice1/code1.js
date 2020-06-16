b9d7e5fc81bf57cca6fa592e8992aec1e923c4b6
Aman Jain <amanjain4103@gmail.com>
2020-06-17 01:25:18 +0530

    adding repository

---
+++ web development/general/practice1/code1.js
@@ -0,0 +1,2 @@
+const name="aman";
+module.exports={"name":name};
---
+++ web development/general/practice2/code2.js
@@ -0,0 +1,5 @@
+const lastName="jain";
+const {name} = require('../practice1/code1');
+module.exports={"lastName":lastName};
+
+console.log(name+" "+lastName)
